2dd7e19f82e0b908dbe5b2d53ed2b134dd398030
shani-dev01 <https://github.com/Sunnychaudhary01>
2026-09-23 12:01:30 +0530

    Feature product sections cards image and price and discounted price fix

--- src/Components/FeatureSection.jsx
+++ src/Components/FeatureSection.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import { IconArrowRight, IconShoppingCart, IconStarFilled, IconTrolley } from "@tabler/icons-react";
+import { IconArrowRight, IconHeart, IconShoppingCart, IconStarFilled, IconTrolley } from "@tabler/icons-react";
 
 function FeatureSection({productsCard}){
 return(
     <>
     <section className="h-[550px] w-full bg-[#FFFFFF] ">
-        <div className="w-full flex items-start justify-between pt-20 px-20">
+        <div className="w-full flex items-start justify-between pt-20 px-13">
           <div className="flex flex-col items-start justify-start">
             <h2 className="text-2xl font-bold text-slate-800">
               Featured Products
@@ -21,32 +21,37 @@ return(
             </Link>
           </div>
         </div>
-        <div className="w-full h-[300px] flex  justify-center gap-8 items-center pt-15 px-10">
+        <div className="w-full h-[300px] flex  justify-center gap-5 items-center pt-15 px-10">
             {productsCard.map((p) =>{
-              console.log(p);
               
-            const discountedPrice = (p.price - (p.price * p.discountPercentage / 100 )).toFixed(2);
-            let cleanCategory = p.category.replace('-', " ").toUpperCase()
+            const indianPrice = Math.round(p.price) * 90;
+            console.log(p.price, p.discountPercentage);
+            
+            const roundedDiscount = Math.round(p.discountPercentage)
+            const discountedPrice = Math.round((indianPrice - (indianPrice * roundedDiscount / 100 )));
+            
+            
             return(
-              <div key={p.id} className="bg-slate-100 px-2 h-[285px] w-[235px] border-[1px] border-[#cccc] shadow-md rounded-xl flex flex-col items-center justify-start gap-4 px-1  hover:-translate-y-2 pt-1 transition 0.3s ease-in-out">
-            <div className="h-[115px] w-full flex items-center justify-center " >
-              <img src={p.thumbnail} className="h-[100%] w-[52%] object-cover " alt="" />
+              <div key={p.id} className=" h-[295px] w-[235px] border-[1px] border-[#cccc] shadow-md rounded-xl flex flex-col items-center justify-start gap-1 hover:-translate-y-2 pt-0 transition 0.3s ease-in-out">
+            <div className="h-[142px] rounded-tl-xl rounded-tr-xl w-full bg-slate-100 flex items-center justify-center relative" >
+              <IconHeart className="absolute right-[15px] top-[15px] text-slate-500 h-[26px] w-[26px] " />
+              <img src={p.thumbnail} className="h-[100%] w-[52%] object-contain " alt="" />
             </div>
-            <div className="h-[85px] w-full px-[10px] flex flex-col items-start justify-center" >
-              <h2 className="text-[#0F172A] font-bold text-[17px] line-clamp-1 "  >{p.title}</h2>
-              <h2 className="flex items-center justify-start gap-2 text-amber-600" >
-                <IconStarFilled className="fill-amber-600 h-[18px] w-[18px]  " /> 4.1k </h2>
-              <div className="flex items-center justify-between text-[17px] gap-7" >
-                <span className="flex gap-2 text-[#0F172A] font-bold">
-                {discountedPrice}
+            <div className="h-[140px] w-full px-[10px] flex flex-col items-start justify-center" >
+              <h2 className="text-[#0f172ada] font-bold text-[17px] line-clamp-1 "  >{p.title}</h2>
+              <h2 className="flex items-center justify-start gap-2 text-amber-600 text-[16px] font-bold " >
+                <IconStarFilled className="fill-amber-400 h-[15px] w-[15px]  " /> {p.rating} k </h2>
+              <div className="w-full flex items-center justify-between text-[17px] gap-5" >
+                <span className="flex gap-2 text-[#0f172ada] font-bold">
+                {'\u20B9'}{discountedPrice}
                 <span className="line-through text-[#94A3B8] font-bold" >
-                {Math.round(p.price) }
+                {'\u20B9'}{Math.round(p.price) * 90  }
                 </span>
                 </span>
                 <span className="h-[22px] w-[65px] bg-emerald-200 text-red-600 text-[13px] flex items-center justify-center rounded-lg " >{ Math.round(p.discountPercentage)}% OFF</span>
               </div>
+            <button className=" h-[32px] flex items-center justify-center gap-2 text-[13px] text-[#FFFFFF] bg-blue-600 w-full rounded-lg mt-4 " > <IconShoppingCart className="h-[18px] w-[18px]" /> Add to Cart</button>
             </div>
-            <button className="flex items-center justify-center gap-2 text-[13px] text-[#FFFFFF] bg-blue-600 w-full rounded-lg h-[35px] " > <IconShoppingCart className="h-[18px] w-[18px]" /> Add to Cart</button>
           </div>
           )}
           )}
